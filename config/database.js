b9b25348a447d77ef3be108b79473988afe838f1
unknown <felipenatale@gmail.com>
2020-12-09 21:50:27 -0300

    Mongo Schema e rotas implementados

--- config/database.js
+++ config/database.js
@@ -1,2 +1,8 @@
 const mongoose = require('mongoose')
+const server = require('./server')
 module.exports = mongoose.connect('mongodb://localhost/db_finance')
+
+mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório."
+mongoose.Error.messages.Number.min = "O '{VALUE}' informado é menor que o limite mínimo de '{MIN}'."
+mongoose.Error.messages.Number.max = "O '{VALUE}' informado é menor que o limite mínimo de '{MAX}'."
+mongoose.Error.messages.String.enum = "O '{VALUE}' não é valido para o atributo '{PATH}'."
